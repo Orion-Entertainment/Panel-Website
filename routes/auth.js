a687b7d49a0986a09097f6b155ccbfba80e0c205
Flabby <flabby@orionlife.net>
2018-07-25 15:39:20 -0400

    debug

--- routes/auth.js
+++ routes/auth.js
@@ -5,6 +5,7 @@ const express = require('express'),
 
 
 router.get('/', function(req, res) {
+    console.log(req.user)
     return res.send(req.user == null ? 'not logged in' : 'hello ' + req.user.username).end();
 });
  

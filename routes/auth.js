510af3ec3ac03fcb9288632a311aa0a4b79b40ba
Flabby <flabby@orionlife.net>
2018-07-25 15:41:24 -0400

    Steam Auth

--- routes/auth.js
+++ routes/auth.js
@@ -5,7 +5,7 @@ const express = require('express'),
 
 
 router.get('/', function(req, res) {
-    console.log(req.session.user)
+    console.log(req.session.SteamUser["_json"].steamid)
     return res.send(req.user == null ? 'not logged in' : 'hello ' + req.user.username).end();
 });
  

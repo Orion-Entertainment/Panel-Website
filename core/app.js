df2a6d77657a59d3856131af9d0fc10776fd8699
Flabby <flabby@orionlife.net>
2018-07-26 18:00:37 -0400

    Home page

--- core/app.js
+++ core/app.js
@@ -73,6 +73,8 @@ app.use((req, res, next) => {
     req.WebTitle = "Orion-Entertainment Panel - ";
     if (req.query.ReturnURL !== undefined) 
         req.session.ReturnURL = req.query.ReturnURL;
+    if (req.session.Account !== undefined)
+        req.Login = true;
     
     next();
 });

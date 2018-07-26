3cab2120f5c415f6ff1bf1008f1a92a8a7e7ceba
Flabby <flabby@orionlife.net>
2018-07-26 13:54:24 -0400

    Steam Login

--- routes/auth.js
+++ routes/auth.js
@@ -2,6 +2,14 @@ const express = require('express'),
 	router = express.Router(),
 	steam = require('steam-login');
 
+RequireLogin = function(redirect) {
+	return function(req, res, next) {
+		if(!req.login)
+			return res.redirect(redirect);
+		next();
+	};
+}
+
 router.get('/', function(req, res) {
     console.log(req.session.SteamUser)
     return res.send(req.user == null ? 'not logged in' : 'hello ' + req.user.username).end();
@@ -12,12 +20,20 @@ router.get('/login', steam.authenticate(), function(req, res) {
 });
  
 router.get('/verify', steam.verify(), function(req, res) {
-	console.log(req.user)
-    return res.send(req.session).end();
+	req.session.Account = {SteamID:req.session.steamUser.steamid};
+	req.user = null;
+	delete req.session.steamUser;
+	req.login = true;
+
+	console.log(req.sesson.Account)
+    return res.send(req.login).end();
 });
  
-router.get('/logout', steam.enforceLogin('/'), function(req, res) {
-    req.logout();
+router.get('/logout', RequireLogin('/'), function(req, res) {
+	delete req.session.Account;
+	req.login = null;
     return res.redirect('/');
 });
+
 module.exports = router;
+module.exports.RequireLogin = RequireLogin;

b4b2d44cf7ab190598219cec87f8dc82017722a1
Flabby <flabby@orionlife.net>
2018-08-06 16:40:15 -0400

    fix

--- public/js/players.full.js
+++ public/js/players.full.js
@@ -218,7 +218,7 @@ function LoadTopCharts(load) {
 /* ---------------- */
 /* /player/killfeed */
 /* ---------------- */
-function LoadTopCharts() {
+function LoadKillFeed() {
     $.ajax({
         async: true,
         type: 'POST',

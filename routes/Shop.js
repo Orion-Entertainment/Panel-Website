e05b3565597b198d04cc0e21df4703b03a093efa
Flabby <flabby@orionlife.net>
2018-08-09 13:46:54 -0400

    Category

--- routes/Shop.js
+++ routes/Shop.js
@@ -24,4 +24,32 @@ router.get('/', async(req, res, next) => {
     }
 });
 
+router.get('/:Category', async(req, res, next) => {
+    try {
+        if (req.params.Category == undefined) {const err = new Error('Not Found');err.status = 404;next(err); return;}
+        else if (req.params.Category == "") {const err = new Error('Not Found');err.status = 404;next(err); return;}
+
+        request.post(
+            'https://panelapi.orion-entertainment.net/v1/shop/category',
+            { json: { 
+                "client_id": await req.APIKey.client_id,
+                "token": await req.APIKey.token,
+                "Category": req.params.Category
+            } },
+            async function (error, response, body) {
+                if (!error && response.statusCode == 200) {
+                    if (body.Error !== undefined) {
+                        if (body.Error == "Category Not Found") {const err = new Error('Not Found');err.status = 404;next(err); return;}
+                        else {return res.render('errorCustom', { error: body.Error });}
+                    } else {
+                        return res.render('./Shop/index', { title: req.WebTitle+'Shop - '+req.params.Category, Data: body });
+                    }
+                } else return res.render('errorCustom', { error: "API: Response Error" });
+            }
+        );
+    } catch (error) {
+        return res.render('errorCustom', { error: error });
+    }
+});
+
 module.exports = router;

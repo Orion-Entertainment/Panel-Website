d67059e32af4af1b000d182fa5bb121e5141779e
Flabby <flabby@orionlife.net>
2018-08-10 21:30:01 -0400

    Test

--- routes/Shop.js
+++ routes/Shop.js
@@ -116,4 +116,141 @@ router.get('/:Category/:Item/Buy', RequireLogin(), async(req, res, next) => {
     }
 });
 
+
+
+var paypal = require('paypal-rest-sdk');
+
+var clientId = 'AV-4DyvKAAaXllJA3zxB1yOtxGc9Sx-wXPHTDAq2U97ebpyH0n92uWTHwSPu3zNxpSqWxq25QfJF1eyZ';
+var secret = 'EAP-_kApLTQcXCjiKKIpmsELdrc4ZFt0xfruOPG_C0s82jPd71nCSaqAC6_lWFgJ2oD9F0kRYRwYbtRd';
+
+paypal.configure({
+  'mode': 'sandbox', //sandbox or live
+  'client_id': clientId,
+  'client_secret': secret
+});
+
+var billingPlanAttribs = {
+    "name": "Food of the World Club Membership: Standard",
+    "description": "Monthly plan for getting the t-shirt of the month.",
+    "type": "fixed",
+    "payment_definitions": [{
+        "name": "Standard Plan",
+        "type": "REGULAR",
+        "frequency_interval": "1",
+        "frequency": "MONTH",
+        "cycles": "11",
+        "amount": {
+            "currency": "USD",
+            "value": "19.99"
+        }
+    }],
+    "merchant_preferences": {
+        "setup_fee": {
+            "currency": "USD",
+            "value": "1"
+        },
+        "cancel_url": "https://panel.orion-entertainment.net/Shop/cancel", ///////////////////////////////////////////////
+        "return_url": "https://panel.orion-entertainment.net/Shop/processagreement", ///////////////////////////////////////////////
+        "max_fail_attempts": "0",
+        "auto_bill_amount": "YES",
+        "initial_fail_amount_action": "CONTINUE"
+    }
+};
+
+var billingPlanUpdateAttributes = [{
+    "op": "replace",
+    "path": "/",
+    "value": {
+        "state": "ACTIVE"
+    }
+}];
+
+paypal.billingPlan.create(billingPlanAttribs, function (error, billingPlan){
+    if (error){
+        console.log(error);
+        throw error;
+    } else {
+        // Activate the plan by changing status to Active
+        paypal.billingPlan.update(billingPlan.id, billingPlanUpdateAttributes, 
+            function(error, response){
+            if (error) {
+                console.log(error);
+                throw error;
+            } else {
+                console.log(billingPlan.id);
+            }
+        });
+    }
+});
+
+router.get('/createagreement', RequireLogin(), async(req, res, next) => {
+    try {billingPlan = req.query.plan;
+
+        var isoDate = new Date();
+        isoDate.setSeconds(isoDate.getSeconds() + 4);
+        isoDate.toISOString().slice(0, 19) + 'Z';
+
+        var billingAgreementAttributes = {
+            "name": "Standard Membership",
+            "description": "Food of the World Club Standard Membership",
+            "start_date": isoDate,
+            "plan": {
+                "id": billingPlan
+            },
+            "payer": {
+                "payment_method": "paypal"
+            },
+            "shipping_address": {
+                "line1": "W 34th St",
+                "city": "New York",
+                "state": "NY",
+                "postal_code": "10001",
+                "country_code": "US"
+            }
+        };
+
+        // Use activated billing plan to create agreement
+        paypal.billingAgreement.create(billingAgreementAttributes, function (
+            error, billingAgreement){
+            if (error) {
+                console.error(error);
+                throw error;
+            } else {
+                //capture HATEOAS links
+                var links = {};
+                billingAgreement.links.forEach(function(linkObj){
+                    links[linkObj.rel] = {
+                        'href': linkObj.href,
+                        'method': linkObj.method
+                    };
+                })
+
+                //if redirect url present, redirect user
+                if (links.hasOwnProperty('approval_url')){
+                    res.redirect(links['approval_url'].href);
+                } else {
+                    console.error('no redirect URI present');
+                }
+            }
+        });
+    } catch (error) {
+        return res.render('errorCustom', { error: error });
+    }
+});
+
+router.get('/processagreement', function(req, res){
+    var token = req.query.token;
+
+    paypal.billingAgreement.execute(token, {}, function (error, 
+        billingAgreement) {
+        if (error) {
+            console.error(error);
+            throw error;
+        } else {
+            console.log(JSON.stringify(billingAgreement));
+            res.send('Billing Agreement Created Successfully');
+        }
+    });
+});
+
 module.exports = router;

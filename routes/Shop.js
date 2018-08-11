a9f3190ec9dcf7344eced606e5e2814b15942ae8
Flabby <flabby@orionlife.net>
2018-08-10 23:09:03 -0400

    fix

--- routes/Shop.js
+++ routes/Shop.js
@@ -145,14 +145,13 @@ router.post('/buy', RequireLogin(), async(req, res, next) => {
 
 router.get('/Success', RequireLogin(), async(req, res, next) => {
     try {
-        var buytoken = req.query.token;
         request.post(
             'https://panelapi.orion-entertainment.net/v1/shop/bought',
             { json: { 
                 "client_id": await req.APIKey.client_id,
                 "token": await req.APIKey.token,
 
-                "buytoken": req.body.buytoken,
+                "buytoken": req.body.token,
                 "payerid": req.body.payerid,
 
                 "Buying": req.session.Account.Buying

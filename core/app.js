265d95597c977f14d49520895d91a979e6d16c43
Flabby <flabby@orionlife.net>
2018-08-13 00:33:00 -0400

    ??

--- core/app.js
+++ core/app.js
@@ -42,11 +42,11 @@ hbs.registerHelper('formatNumber', function(value) {
 });
 const moment = require('moment');
 hbs.registerHelper('formatDate', async function(date) {
-    Data = await moment(date).format('YYYY/MM/DD HH:mm:ss');
+    const Data = await moment(date).format('YYYY/MM/DD HH:mm:ss');
     return Data;
 });
 hbs.registerHelper('formatDateSmall', async function(date) {
-    Data = await moment(date).format('YYYY/MM/DD HH:mm');
+    const Data = await moment(date).format('YYYY/MM/DD HH:mm');
     return Data;
 });
 hbs.registerHelper("math", function(lvalue, operator, rvalue, options) {

5b795b288a6c4bf2a0d6d6518898771eeaf6f6b1
Flabby <flabby@orionlife.net>
2018-08-13 01:02:53 -0400

    fix

--- public/js/changelogs.full.js
+++ public/js/changelogs.full.js
@@ -4,6 +4,6 @@
 function ChangeLogs(changelogs) {
     for (i = 0; i < changelogs.length; i++) {
         changelog = changelogs[i];
-        $('.centercolumn .centerDIV').append('<div class="ChangelogCard"><h2>'+changelog["Name"]+'</h2><h5>'+changelog["Category"]+', '+moment(changelog["Time"]).format('YYYY/MM/DD HH:mm')+'</h5></div>');
+        $('#ChangeLogs').append('<div class="ChangelogCard"><h2>'+changelog["Name"]+'</h2><h5>'+changelog["Category"]+', '+moment(changelog["Time"]).format('YYYY/MM/DD HH:mm')+'</h5></div>');
     }
 }

02db6eb6af4f451a94bb073e5b08aa89fbaf3812
Flabby <flabby@orionlife.net>
2018-08-13 01:35:24 -0400

    oof

--- routes/index.js
+++ routes/index.js
@@ -114,7 +114,7 @@ router.get('/Changelog', async(req, res, next) => {
             async function (error, response, body) {
                 if (!error && response.statusCode == 200) {
                     if (body.Error !== undefined) return res.render('errorCustom', { error: body.Error });
-                    else return res.render('./Changelog/Index', { title: req.WebTitle+'Changelog Admin', Login: req.Login, Admin:Admin, Data: JSON.stringify(body) });
+                    else return res.render('./Changelog/Index', { title: req.WebTitle+'Changelog Admin', Login: req.Login, Admin:Admin, Data:body });
                 } else return res.render('errorCustom', { error: "API: Response Error" });
             }
         );
